5a5ec2ee45e8a136da296ac5df840ebb0b93fcf5
Dino <149540005+SrDino0@users.noreply.github.com>
2024-09-11 02:53:28 -0300

    Delete portfolio directory

--- portfolio/atividades-js/atv4.js
+++
@@ -1,9 +0,0 @@
-var numeros = [];
-
-for (let i = 1; i <= 10; i++) {
-    numeros[i] = i
-}
-
-numeros.forEach(num=>{
-    document.write("<h3>"+num+"<h3>");
-});
--- portfolio/script.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('.menu');
-const navMenu = document.querySelector('.nav-menu');
-
-menu.addEventListener('click', () => {
-  navMenu.classList.toggle('hide');
-});
